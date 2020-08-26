39882e1d3617d0dcd4ce92a9229714874ef355c9
Jonathan Xiao <jonathanx111@gmail.com>
2020-08-26 20:54:56 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ def find_min_in_nested_arrays(src)
     longest_num = ""
     while rowidx < src[eleidx].count do
       if src[eleidx][rowidx] > longest_num.to_i
-        longest_string =  scr[eleidx][rowidx]
+        longest_string =  src[eleidx][rowidx]
       end
       rowidx += 1 
     end
